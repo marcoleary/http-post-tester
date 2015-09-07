c30fdc3cc5483569e702de0166036d282a5d52a8
Marc O'Leary <marcoleary@gmail.com>
2015-09-07 17:52:30 +0100

    Initial commit of skeleton code

--- classes/HttpPostTester.php
+++ classes/HttpPostTester.php
@@ -4,11 +4,27 @@ namespace HttpPostTester;
 
 class HttpPostTester
 {
+	protected $strPathToData = __DIR__ . '/../data/';
+
 	public function __construct()
 	{
+		
 	}
 
 	public function init()
 	{
+		$resHandle = opendir($this->strPathToData);
+
+		while (false !== ($strFile = readdir($resHandle))) {
+			if (in_array($strFile, ['.', '..'])) {
+				continue;
+			}
+
+			if ('csv' !== pathinfo($strFile, PATHINFO_EXTENSION)) {
+				continue;
+			}
+
+			// do stuff
+		}
 	}
 }
